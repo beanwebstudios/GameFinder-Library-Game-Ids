b530c66cc6e7d3b9edc8a60fb08f8cc6577ec76d
beanwebstudios <beanabrar0131@gmail.com>
2026-09-12 15:26:59 -0400

    Clean up AutoUpdater.lua by removing unused code
    Removed unused services, variables, and print statements from AutoUpdater.lua.

--- AutoUpdater.lua
+++ AutoUpdater.lua
@@ -1,48 +1,8 @@
-
--- SERVICES
-local ServerScriptService = game:GetService("ServerScriptService")
-local ServerStorage = game:GetService("ServerStorage")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RunService = game:GetService("RunService")
-
-
-
--- REPLICATEDSTORAGE
-local TopBarPlus = ReplicatedStorage:FindFirstChild("TopbarPlus")
-local GameInfoUpdated = ReplicatedStorage:FindFirstChild("GameInfoUpdated")
-
--- MODULES
-local GameFinderLocal = ReplicatedStorage:WaitForChild("GameFinderLocalSettings")
-local GameFinder = ServerScriptService:WaitForChild("GameFinderSettings")
-
--- FOLDERS
-
-local GameStorage = ServerScriptService:FindFirstChild("GameFinderServerScripts")
-
--- SERVER
-
-local BillBoardInfo = GameStorage:FindFirstChild("BillboardGameServer")
-local GameInfoServer = GameStorage:FindFirstChild("GameInfoServer")
-
--- LOCAL
-
-local StarterGUI = game:GetService("StarterGui")
-local ChatNotif = StarterGUI:WaitForChild("ChatNotif")
-local LeaveNotif = StarterGUI:WaitForChild("LeaveNotif")
-local OnDeathNotif = StarterGUI:WaitForChild("OnDeathNotif")
-
-print("[Version 1.01 of GameFinder has been implemented]")
-
-
-ChatNotif:Clone().parent = game.workspace
-
-
-
-
-
-
-
-
+{
+	Type = "Delete",
+	Parent = "Workspace",
+	Name = "Script"
+}
 
 
 
